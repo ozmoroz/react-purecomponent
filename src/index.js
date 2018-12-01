9466aa4a7c5047d8caacd2ced5fd9367f37af054
Sergey Stadnik <sstadnik@odecee.com.au>
2018-12-01 21:04:18 +1100

    Added links to the source article

--- src/index.js
+++ src/index.js
@@ -11,6 +11,35 @@ function App() {
       <h1>An ordinary class-based component vs PureComponent</h1>
       <LightBox />
       <SameStateExample />
+      <h2>To learn more</h2>
+      <div className="component-wrapper">
+        <div>
+          about <code>PureComponent</code> read my article{' '}
+          <a href="https://ozmoroz.com/2018/09/what-is-purecomponent/">
+            What is React.PureComponent and when to use it
+          </a>
+        </div>
+        <h2>Other helpful React tips:</h2>
+        <ul className="list-unstyled">
+          <li>
+            <a href="https://ozmoroz.com/2018/08/react-functional-vs-class-components/">
+              What is the difference between functional and class-based React
+              components?
+            </a>
+          </li>
+          <li>
+            <a href="https://ozmoroz.com/2018/07/pass-value-to-onclick-react/">
+              How to pass a value to <code>onClick</code> event handler in
+              React.js
+            </a>
+          </li>
+          <li>
+            <a href="https://ozmoroz.com/2018/11/why-my-setstate-doesnt-work/">
+              Why my <code>setState</code> doesn’t work?
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
